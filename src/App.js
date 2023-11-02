b54dd650acd7ff77cca7420186e48a9db04fe12a
mhdfahad1 <mhdfahad53@gmail.com>
2023-11-02 19:12:28 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -11,17 +11,20 @@ function App() {
     setvalue("")
   }
   const Calculate = () => {
-    try{
-      const result = eval(value)
-
-      setvalue(result)
-    }catch{
+    try {
+      const result = parseInt(value)
+      setvalue(eval(result))
+    } catch (error) {
+      console.log(error);
       setvalue("SYNTAX ERROR!")
     }
   }
 
-  const removeLast=()=>{
-    setvalue(value.slice(0,-1))
+  const removeLast = () => {
+    if (value) {
+      setvalue(value.slice(0, -1))
+    }
+    // setvalue(value.slice(0,-1))
   }
   return (
     <>
@@ -30,23 +33,23 @@ function App() {
           <div><input placeholder='0' value={value} id='inputbox' type="text" readOnly /></div>
           <div className='firstrow'>
             <div onClick={() => clearInput()} className='clear  border'>c</div>
-            <div onClick={()=>removeLast()} className='backButton border'><i className="fa-solid fa-delete-left" style={{ color: 'black' }}></i></div>
+            <div onClick={() => removeLast()} className='backButton border'><i className="fa-solid fa-delete-left" style={{ color: 'black' }}></i></div>
             <div onClick={() => display("%")} className='modulus border'>%</div>
-            <div onClick={() => display("+")} className='plus border'><i className="fa-solid fa-plus" style={{color:'black'}}></i></div>
+            <div onClick={() => display("+")} className='plus border'><i className="fa-solid fa-plus" style={{ color: 'black' }}></i></div>
           </div>
 
           <div className='firstrow'>
             <div onClick={() => display("7")} className='buttons border'>7</div>
             <div onClick={() => display("8")} className='buttons border'>8</div>
             <div onClick={() => display("9")} className='buttons border'>9</div>
-            <div onClick={() => display("-")} className='minus border'><i className="fa-solid fa-minus" style={{color:'black'}}></i></div>
+            <div onClick={() => display("-")} className='minus border'><i className="fa-solid fa-minus" style={{ color: 'black' }}></i></div>
           </div>
 
           <div className='firstrow'>
             <div onClick={() => display("4")} className='buttons border'>4</div>
             <div onClick={() => display("5")} className='buttons border'>5</div>
             <div onClick={() => display("6")} className='buttons border'>6</div>
-            <div onClick={() => display("*")} className='multiply border'><i className="fa-solid fa-xmark" style={{color:'black'}}></i></div>
+            <div onClick={() => display("*")} className='multiply border'><i className="fa-solid fa-xmark" style={{ color: 'black' }}></i></div>
           </div>
 
           <div className='firstrow'>
